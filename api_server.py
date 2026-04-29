8a9546723cfaa1d76741a01616d8c2baca2adbc4
Ram <jandhyala.ramkrishna@gmail.com>
2026-04-29 17:37:07 +0530

    trigger test

--- api_server.py
+++ api_server.py
@@ -1,19 +1,40 @@
+
 from flask import Flask, request, jsonify
+import mysql.connector
 
 app = Flask(__name__)
 
-data_store = []
+# MySQL connection
+
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="qa_user",
+    password="Qa@12345",
+    database="ros2_data"
+)
+
+cursor = db.cursor()
 
 @app.route('/data', methods=['POST'])
 def receive_data():
     data = request.json
-    data_store.append(data)
-    return jsonify({"status": "received", "data": data})
+    message = data.get("message")
+
+    # Insert into DB
+    sql = "INSERT INTO messages (message) VALUES (%s)"
+    cursor.execute(sql, (message,))
+    db.commit()
+
+    return jsonify({"status": "stored", "message": message})
 
 @app.route('/data', methods=['GET'])
 def get_data():
-    return jsonify(data_store)
+    cursor.execute("SELECT * FROM messages")
+    result = cursor.fetchall()
+
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-# test change
+# trigger test
